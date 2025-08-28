371f68f6172e892ae578055a6af2cd9b540085db
nikhils257 <nikhilwakale10@gmail.com>
2025-08-28 19:25:26 +0530

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -1,1 +1,50 @@
-code
+
+#include <stdio.h>
+
+
+
+// Simple Calculator Program by Nikhil //
+// Diploma in IT - FY Project //
+// I Just Did This To Learn Code //
+
+
+int main() {
+    float num1, num2, result;
+    char op;
+
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &op);
+
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+
+    switch(op) {
+        case '+':
+            result = num1 + num2;
+            printf("Result = %.2f\n", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("Result = %.2f\n", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("Result = %.2f\n", result);
+            break;
+        case '/':
+            if(num2 != 0) {
+                result = num1 / num2;
+                printf("Result = %.2f\n", result);
+            } else {
+                printf("Error! Division by zero.\n");
+            }
+            break;
+        default:
+            printf("Invalid operator!\n");
+    }
+
+    return 0;
+}
